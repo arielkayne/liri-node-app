0737c68e1d035de93057a256cbde336331494dfa
akayne <15270231+arielkayne@users.noreply.github.com>
2017-12-06 20:49:04 -0600

    first commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'U20GeFi5ST7ZSfMsab8R4W2Nj',
+  consumer_secret: 'yhibU1qRtUlSukfCX5jlhkWwi5EIl2awxGm58kLJHkPqULhZ78',
+  access_token_key: '938583801432768513-i0z4DN6BuSWaQnGzk5We7GCGZRbebm4',
+  access_token_secret: '7yAXAIrWyX0xaw6kVkfwovFWlD52a5DDmoFd0HutfRxGI',
+}
+
+module.exports = twitterKeys;
